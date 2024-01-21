947d46c8dd7a0adc79fabcfb2fb8c0fe4614b6c9
Samet Koleoglu <sametkoleoglu@192.168.1.103>
2024-01-21 11:21:09 +0300

    Techcareer_Reat_Native_ders_2_odev

--- App.js
+++ App.js
@@ -1,11 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { FlatList, SafeAreaView, StyleSheet, Text, View } from "react-native";
 
 export default function App() {
+  const [datas, setDatas] = useState(null);
+
+  // useEffect kullanarak veri çekme işlemini gerçekleştir
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        // Veriyi çekmek için fetch kullan
+        const response = await fetch(
+          "https://northwind.vercel.app/api/suppliers"
+        );
+
+        // Json formatına çevir
+        const data = await response.json();
+
+        // State'i güncelle
+        setDatas(data);
+      } catch (error) {
+        console.error("Veri çekme hatası:", error);
+      }
+    };
+
+    // Component yüklendiğinde veriyi çek
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+      <FlatList
+        data={datas}
+        renderItem={({ item }) => {
+          return item.address.country === "USA" ? (
+            <SafeAreaView
+              style={{
+                flexDirection: "row",
+                gap: 10,
+                marginVertical: 10,
+                backgroundColor: "red",
+              }}
+            >
+              <Text>{item.companyName}</Text>
+              <Text>{item.address.country}</Text>
+            </SafeAreaView>
+          ) : (
+            <SafeAreaView
+              style={{
+                flexDirection: "row",
+                gap: 10,
+                marginVertical: 10,
+              }}
+            >
+              <Text>{item.companyName}</Text>
+              <Text>{item.address.country}</Text>
+            </SafeAreaView>
+          );
+        }}
+        showsVerticalScrollIndicator={false}
+      />
     </View>
   );
 }
@@ -13,8 +68,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
